1f5b5fc1e91709c62b8617444714531afc879237
AlitaBernachot <alita.bernachot@camptocamp.com>
2025-08-11 16:34:50 +0200

    feat: add 2d back button

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,53 @@
-import type { VcsPlugin, VcsUiApp, PluginConfigEditor } from '@vcmap/ui';
+import {
+  type VcsPlugin,
+  type VcsUiApp,
+  type PluginConfigEditor,
+  ButtonLocation,
+} from '@vcmap/ui';
+import { fromLonLat } from 'ol/proj';
 import { name, version, mapVersion } from '../package.json';
 
-type PluginConfig = Record<never, never>;
+type PluginConfig = {
+  pathTo2dGeoportal: string,
+  tabId: string
+}
 type PluginState = Record<never, never>;
 
 type MyPlugin = VcsPlugin<PluginConfig, PluginState>;
+
+const zoomToCesiumAltitude = {
+  9: 350000,
+  10: 180000,
+  11: 100000,
+  12: 40000,
+  13: 18000,
+  14: 9000,
+  15: 6000,
+  16: 3500,
+  17: 1900,
+  18: 900,
+  19: 600,
+}
+
+function getZoomFromAltitude(altitude: number) {
+  let closestZoom = 9;
+  let smallestDiff = Infinity;
+
+  for (const [zoom, alt] of Object.entries(zoomToCesiumAltitude)) {
+    const diff = Math.abs(altitude - alt);
+    if (diff < smallestDiff) {
+      smallestDiff = diff;
+      closestZoom = parseInt(zoom);
+    }
+  }
+
+  return closestZoom;
+}
 
 export default function plugin(
   config: PluginConfig,
   baseUrl: string,
 ): MyPlugin {
-  // eslint-disable-next-line no-console
-  console.log(config, baseUrl);
   return {
     get name(): string {
       return name;
@@ -23,20 +59,39 @@ export default function plugin(
       return mapVersion;
     },
     initialize(vcsUiApp: VcsUiApp, state?: PluginState): Promise<void> {
-      // eslint-disable-next-line no-console
-      console.log(
-        'Called before loading the rest of the current context. Passed in the containing Vcs UI App ',
-        vcsUiApp,
-        state,
+      const action = {
+        name: '2D',
+        title: 'back2d.title',
+        icon: '$vcs2d',
+        callback: async () => {
+           const state = await vcsUiApp.getState(true);
+
+          const [lon, lat, alt] = <number[]>state.activeViewpoint?.cameraPosition;
+
+          const coordinates = fromLonLat([lon, lat]);
+          const x = Math.round(coordinates[0]);
+          const y = Math.round(coordinates[1]);
+          const zoom = getZoomFromAltitude(alt);
+     
+          const link = document.createElement('a');
+          link.href = `${config.pathTo2dGeoportal}?X=${Math.round(x)}&Y=${Math.round(y)}&zoom=${zoom}&version=3`;
+          link.target = config.tabId;
+          link.click();
+          
+          console.log(link.href)
+        },
+      };
+
+      vcsUiApp.navbarManager.add(
+        { id: 'back-2D', action },
+        name,
+        ButtonLocation.MAP,
+        { desktop: true, tablet: true, mobile: true },
       );
+
       return Promise.resolve();
     },
     onVcsAppMounted(vcsUiApp: VcsUiApp): void {
-      // eslint-disable-next-line no-console
-      console.log(
-        'Called when the root UI component is mounted and managers are ready to accept components',
-        vcsUiApp,
-      );
     },
     /**
      * should return all default values of the configuration
@@ -58,8 +113,6 @@ export default function plugin(
      * @returns {PluginState}
      */
     getState(forUrl?: boolean): PluginState {
-      // eslint-disable-next-line no-console
-      console.log('Called when collecting state, e.g. for create link', forUrl);
       return {
         prop: '*',
       };
@@ -70,9 +123,28 @@ export default function plugin(
     getConfigEditors(): PluginConfigEditor<object>[] {
       return [];
     },
-    destroy(): void {
-      // eslint-disable-next-line no-console
-      console.log('hook to cleanup');
+    destroy(): void {},
+    i18n: {
+      de: {
+        back2d: {
+          title: 'Zurück zum Luxemburger Geoportal',
+        },
+      },
+      en: {
+        back2d: {
+          title: 'Back to the Luxembourg geoportal',
+        },
+      },
+      fr: {
+        back2d: {
+          title: 'Retour au géoportail du Luxembourg',
+        },
+      },
+      lb: {
+        back2d: {
+          title: 'Zeréck op de Lëtzebuerger Geoportal',
+        },
+      },
     },
   };
 }
